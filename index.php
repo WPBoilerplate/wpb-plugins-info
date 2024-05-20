9d92bb7eaaddcebc44a7b207173c05e78dcf1d41
raftaar1191 <raftaar1191@gmail.com>
2024-05-20 16:02:40 +0530

    Update code

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * BuddyBoss Compatibility Integration Class.
+ *
+ * @since BuddyBoss 1.1.5
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Check if the class does not exits then only allow the file to add
+ */
+if( ! class_exists( 'AcrossWP_Plugins_Info' ) ) {
+	/**
+	 * Fired during plugin licences.
+	 *
+	 * This class defines all code necessary to run during the plugin's licences and update.
+	 *
+	 * @since      0.0.1
+	 * @package    AcrossWP_Plugins_Info
+	 * @subpackage AcrossWP_Plugins_Info/includes
+	 * @author     AcrossWP <contact@acrosswp.com>
+	 */
+	class AcrossWP_Plugins_Info {
+
+		/**
+		 * The single instance of the class.
+		 *
+		 * @var AcrossWP_Plugins_Info
+		 * @since 0.0.1
+		 */
+		protected static $_instance = null;
+
+		/**
+		 * Main AcrossWP_Plugins_Info Instance.
+		 *
+		 * Ensures only one instance of WooCommerce is loaded or can be loaded.
+		 *
+		 * @since 0.0.1
+		 * @static
+		 * @see AcrossWP_Plugins_Info()
+		 * @return AcrossWP_Plugins_Info - Main instance.
+		 */
+		public static function instance() {
+			if ( is_null( self::$_instance ) ) {
+				self::$_instance = new self();
+			}
+			return self::$_instance;
+		}
+
+		/**
+		 * Get the vendor path of composer
+		 * 
+		 * @return string Path of the vendor dir
+		 */
+		function get_vendor_path() {
+			return \SzepeViktor\Composer\PackagePath::getVendorPath();	
+		}
+
+		/**
+		 * Get the plugin path
+		 * 
+		 * @return string Path of the plugins
+		 */
+		function get_plugin_path() {
+			return dirname( $this->get_vendor_path() );
+		}
+
+		/**
+		 * Get the plugin path
+		 * 
+		 * @return string Path of the plugins
+		 */
+		function get_full_plugin_path() {
+			return $this->get_plugin_path() . '/' . $this->get_plugin_file_name() . '.php';
+		}
+
+		/**
+		 * Get the plugin path
+		 * 
+		 * @return string Path of the plugins
+		 */
+		function get_plugin_file_name() {
+			return basename( $this->get_plugin_path() );
+		}
+
+		/**
+		 * Get the plugin path
+		 * 
+		 * @return string Path of the plugins
+		 */
+		function get_block_path() {
+			return $this->get_plugin_path() . '/build';
+		}
+	}
+
+	AcrossWP_Plugins_Info::instance();
+}
